bdf4f27e170330e67a3a37e6ac282475e153cb66
samisams1 <forsamisams@gmail.com>
2022-08-06 11:49:10 +0300

    product crud

--- src/order/order.resolver.ts
+++ src/order/order.resolver.ts
@@ -13,6 +13,14 @@ export class OrderResolver {
     async createOrder(@Args('input') input: OrderType) {
         return this.orderService.create(input);
     }
+    @Mutation(returns => OrderType)
+    async updateOrder(@Args('id') id:string,@Args('input') input:OrderType) {
+        return  this.orderService.update(id,input);
+    }
+    @Mutation(returns => OrderType)
+    async deleteOrder(@Args('id') id:string) {
+    return this.orderService.delete(id);
+ }
 }
 
 
